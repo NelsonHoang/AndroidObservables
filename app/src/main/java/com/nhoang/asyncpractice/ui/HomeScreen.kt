58e8d92d0fa652e34e64107ce09beec33a9fb7d8
NelsonH <nelson.hoang@gmail.com>
2024-12-18 09:47:22 -0800

    Implement StateFlow and SharedFlow example

--- app/src/main/java/com/nhoang/asyncpractice/ui/HomeScreen.kt
+++ app/src/main/java/com/nhoang/asyncpractice/ui/HomeScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
@@ -18,10 +19,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(homeViewModel: HomeViewModel) {
     val liveDataText by homeViewModel.liveDataText.observeAsState()
+    val stateFlowText by homeViewModel.stateFlowText.collectAsState()
+    val sharedFlowText by homeViewModel.sharedFlowText.collectAsState("Hello World")
 
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
@@ -39,6 +44,41 @@ fun HomeScreen(homeViewModel: HomeViewModel) {
             shape = RoundedCornerShape(50)
         ) {
             Text("Live Data")
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = stateFlowText,
+            fontSize = 24.sp
+        )
+        OutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp),
+            onClick = {
+                homeViewModel.triggerStateFlow()
+            },
+            border = BorderStroke(2.dp, Color.LightGray),
+            shape = RoundedCornerShape(50)
+        ) {
+            Text("State Flow")
+        }
+        Text(
+            text = sharedFlowText,
+            fontSize = 24.sp
+        )
+        OutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp),
+            onClick = {
+                homeViewModel.viewModelScope.launch {
+                    homeViewModel.triggerSharedFlow()
+                }
+            },
+            border = BorderStroke(2.dp, Color.LightGray),
+            shape = RoundedCornerShape(50)
+        ) {
+            Text("Shared Flow")
         }
     }
 }
--- app/src/main/java/com/nhoang/asyncpractice/ui/HomeViewModel.kt
+++ app/src/main/java/com/nhoang/asyncpractice/ui/HomeViewModel.kt
@@ -2,13 +2,30 @@ package com.nhoang.asyncpractice.ui
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class HomeViewModel : ViewModel() {
     private val _liveDataText = MutableLiveData("Hello World")
     val liveDataText = _liveDataText
 
+    private val _stateFlowText = MutableStateFlow("Hello World")
+    val stateFlowText = _stateFlowText.asStateFlow()
+
+    private val _sharedFlowText = MutableSharedFlow<String>()
+    val sharedFlowText =  _sharedFlowText.asSharedFlow()
+
     fun triggerLiveData() {
         _liveDataText.value = "Live Data"
     }
 
+    fun triggerStateFlow() {
+        _stateFlowText.value = "State Flow"
+    }
+
+    suspend fun triggerSharedFlow() {
+        _sharedFlowText.emit("Shared Flow")
+    }
 }
